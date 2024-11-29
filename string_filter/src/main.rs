fb6678f901c37295fe70786271f6d057625ac9c0
Lysander Poone <mundanesurrealism@gmail.com>
2024-11-29 15:16:13 +0800

    .

--- string_filter/src/main.rs
+++ string_filter/src/main.rs
@@ -3,7 +3,9 @@ use polars::prelude::*;
 
 fn main() -> PolarsResult<()> {
     let a = Series::new("a".into(), [1, 2, 3, 4]);
+    println!("{a:?}");
     let b = Series::new("b".into(), ["one", "two", "three", "four"]);
+    println!("{b:?}");
     let df = DataFrame::new(vec![Column::Series(a), Column::Series(b)])?
         .lazy()
         .filter(col("b").str().starts_with(lit("t")))
