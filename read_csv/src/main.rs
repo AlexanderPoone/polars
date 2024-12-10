79999f684173ffe07bd1b078458085f0552e5713
Lysander Poone <mundanesurrealism@gmail.com>
2024-12-10 12:01:43 +0800

    .

--- read_csv/src/main.rs
+++ read_csv/src/main.rs
@@ -1,9 +1,10 @@
+use std::path::PathBuf;
+
 use polars::io::mmap::MmapBytesReader;
 use polars::prelude::*;
 
 fn main() -> PolarsResult<()> {
-    let file = std::fs::File::open("../datasets/foods1.csv")
-        .unwrap();
+    let file = std::fs::File::open("../datasets/foods1.csv").unwrap();
     let file = Box::new(file) as Box<dyn MmapBytesReader>;
     let _df = CsvReader::new(file)
         // .with_separator(b'|')
@@ -11,6 +12,36 @@ fn main() -> PolarsResult<()> {
         // .with_chunk_size(10)
         .batched(None)
         .unwrap();
+
+    let _dff = CsvReadOptions::default()
+        .with_has_header(true)                             // default is true
+        .with_n_rows(Some(10))
+        .with_columns(Some(                                              // `!vec` is not needed here
+            [
+                PlSmallStr::from_str("category"),                        // Good optimisation via `PlSmallStr`
+                PlSmallStr::from_str("calories"),
+            ]
+            .into(),
+        ))
+        // .with_skip_rows_after_header(10)
+        .with_rechunk(true)
+        .with_chunk_size(10)
+        .with_parse_options(
+            CsvParseOptions::default()                   // Delimiter is under `CsvParseOptions`
+                .with_separator(b' ')
+                .with_try_parse_dates(true)
+                .with_null_values(Some(NullValues::Named(vec![(         // `!vec` is needed here
+                    PlSmallStr::from_str("category"),
+                    PlSmallStr::from_str("vegetables"),
+                ), (
+                    PlSmallStr::from_str("calories"),
+                    PlSmallStr::from_str("-1"),
+                )]))),
+        )
+        .try_into_reader_with_file_path(Some(PathBuf::from("../datasets/foods4.csv")))?
+        .finish()?
+        .drop_nulls::<String>(None)?;                            // <String> is needed here
+    println!("{_dff:?}");
 
     // write_other_formats(&mut df)?;
     Ok(())
