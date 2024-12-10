ce22f486df71550f56b8de09aae492dd0c41586b
Lysander Poone <mundanesurrealism@gmail.com>
2024-12-10 15:12:06 +0800

    .

--- read_csv/src/main.rs
+++ read_csv/src/main.rs
@@ -14,33 +14,49 @@ fn main() -> PolarsResult<()> {
         .unwrap();
 
     let _dff = CsvReadOptions::default()
-        .with_has_header(true)                             // default is true
+        .with_has_header(true) // default is true
         .with_n_rows(Some(10))
-        .with_columns(Some(                                              // `!vec` is not needed here
+        .with_columns(Some(
+            // `!vec` is not needed here
             [
-                PlSmallStr::from_str("category"),                        // Good optimisation via `PlSmallStr`
+                PlSmallStr::from_str("category"), // Good optimisation via `PlSmallStr`
                 PlSmallStr::from_str("calories"),
             ]
             .into(),
         ))
         // .with_skip_rows_after_header(10)
         .with_rechunk(true)
-        .with_chunk_size(10)
+        .with_chunk_size(1)
         .with_parse_options(
-            CsvParseOptions::default()                   // Delimiter is under `CsvParseOptions`
+            CsvParseOptions::default() // Delimiter is under `CsvParseOptions`
                 .with_separator(b' ')
                 .with_try_parse_dates(true)
-                .with_null_values(Some(NullValues::Named(vec![(         // `!vec` is needed here
-                    PlSmallStr::from_str("category"),
-                    PlSmallStr::from_str("vegetables"),
-                ), (
-                    PlSmallStr::from_str("calories"),
-                    PlSmallStr::from_str("-1"),
-                )]))),
+                .with_null_values(Some(NullValues::Named(vec![
+                    (
+                        // `!vec` is needed here
+                        PlSmallStr::from_str("category"),
+                        PlSmallStr::from_str("vegetables"),
+                    ),
+                    (PlSmallStr::from_str("calories"), PlSmallStr::from_str("-1")),
+                ]))),
         )
         .try_into_reader_with_file_path(Some(PathBuf::from("../datasets/foods4.csv")))?
         .finish()?
-        .drop_nulls::<String>(None)?;                            // <String> is needed here
+        .drop_nulls::<String>(None)? // <String> is needed here
+        .with_row_index("Id".into(), None)?;
+
+    // cf. iterrows() BELOW
+    let mut row = _dff.get_row(0)?; // pass row by reference, mutable
+    let col = _dff.get_column_index("category").unwrap(); //  the `?` operator can only be used on `Result`s, not `Option`s, in a function that returns `Result`
+    let mut category = &row.0[col]; // .0 turns the pl::Row object into a Vec
+    println!("{row:?}          {category:?}");
+    for idx in 1.._dff.height() {
+        let _ = _dff.get_row_amortized(idx, &mut row); // pass row by reference,
+        category = &row.0[col]; // .0 turns the pl::Row object into a Vec
+        println!("{row:?}          {category:?}");
+    }
+    // cf. iterrows() ABOVE
+
     println!("{_dff:?}");
 
     // write_other_formats(&mut df)?;
